dbb8800e71f6e459da8e0958eaf38d701d56fb4d
星梦喵吖 <starsdream00@icloud.com>
2023-05-10 13:24:23 +0800

    修复

--- src/Functions/ExportFunc.cs
+++ src/Functions/ExportFunc.cs
@@ -6,12 +6,9 @@ internal static class ExportFunc
 {
     internal static void Setup()
     {
-        _ = RemoteCallAPI.ExportAs(Main.pluginName, "SendMessageAsync",
-            (long chatId, int messageThreadId, string message, int reply) =>
-            {
-                _ = Main.BotClient.SendMessageAsync(chatId, messageThreadId, message, reply);
-            });
-        _ = RemoteCallAPI.ExportAs(Main.pluginName, "SendMessage",
-            (long chatId, int messageThreadId, string message, int reply) => Main.BotClient.SendMessageAsync(chatId, messageThreadId, message, reply).Wait());
+        _ = RemoteCallAPI.ExportAs(Main.pluginName, "SendMessage", (string message) =>
+        {
+            _ = Main.BotClient.SendMessageAsync(Main.Config.ChatId, Main.Config.MessageThreadId, message);
+        });
     }
 }
