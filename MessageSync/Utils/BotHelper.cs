177d32d1e850ecbeddce635bb94ffb70c119d745
星梦 <starsdream00@icloud.com>
2023-07-16 23:55:06 +0800

    修复

--- MessageSync/Utils/BotHelper.cs
+++ MessageSync/Utils/BotHelper.cs
@@ -1,19 +1,19 @@
 using System.Globalization;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace MessageSync.Utils;
 internal static class BotHelper
 {
-    internal static async Task SendMessageAsync(this ITelegramBotClient botClient, long chatId, int messageThreadId, string message, int? reply = default)
+    internal static async Task<Message> SendMessageAsync(this ITelegramBotClient botClient, long chatId, int messageThreadId, string message, int? reply = default)
     {
         while (true)
         {
             try
             {
-                await botClient.SendTextMessageAsync(chatId, message, messageThreadId: (await botClient.GetChatAsync(chatId)).IsForum ?? false ? messageThreadId : default, parseMode: ParseMode.MarkdownV2, replyToMessageId: reply);
-                break;
+                return await botClient.SendTextMessageAsync(chatId, message, messageThreadId: (await botClient.GetChatAsync(chatId)).IsForum ?? false ? messageThreadId : default, parseMode: ParseMode.MarkdownV2, replyToMessageId: reply);
             }
             catch (ApiRequestException ex) when (ex.Message.Contains("Too Many Requests")) { }
             catch (ApiRequestException ex)
@@ -35,6 +35,7 @@ internal static class BotHelper
                 Main.Logger.Debug.WriteLine(ex);
             }
         }
+        return default;
     }
     internal static bool WriteAllException(this AggregateException ex, string message)
     {
