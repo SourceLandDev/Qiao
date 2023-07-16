93337c03ce7d210990fae617ded5917944854abb
星梦 <starsdream00@icloud.com>
2023-07-16 16:51:19 +0800

    修复

--- MessageSync/Utils/APIHelper.cs
+++ MessageSync/Utils/APIHelper.cs
@@ -7,11 +7,11 @@ internal static class APIHelper
 {
     public static string GetUserName(Player player)
     {
-        if (!RemoteCallAPI.HasFunc("UserName", "GetFromXuid"))
+        if (!RemoteCallAPI.HasFunc("UserName", "Get"))
         {
             return player.RealName;
         }
-        RemoteCallAPI.CallbackFn method = RemoteCallAPI.ImportFunc("UserName", "GetFromXuid");
-        return method(new() { player.Xuid });
+        RemoteCallAPI.CallbackFn method = RemoteCallAPI.ImportFunc("UserName", "Get");
+        return method(new() { player });
     }
 }
